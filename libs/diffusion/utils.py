372805d36a5f841ac61156d0c7466e714bce3906
Brayan Monroy <brayansneider22@gmail.com>
2023-10-24 11:02:15 -0500

    Update utils.py

--- libs/diffusion/utils.py
+++ libs/diffusion/utils.py
@@ -1,3 +1,12 @@
+# -----------------------------------------------------------------------------
+# Original Source: DDPMs from Scratch
+# GitHub Repository: spmallick/learnopencv
+# Link: https://github.com/spmallick/learnopencv/tree/master/Guide-to-training-DDPMs-from-Scratch
+# -----------------------------------------------------------------------------
+# Description:
+# Train Denoising Diffusion Probabilisitic Models from scratch on the Flowers and MNIST dataset.
+# -----------------------------------------------------------------------------
+
 import os
 import torch 
 import torchvision
