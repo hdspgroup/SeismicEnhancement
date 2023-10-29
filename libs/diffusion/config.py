bfbdd0b477a4b2afe625ae2ec81a43853ff839f2
Brayan Monroy <brayansneider22@gmail.com>
2023-10-29 14:53:10 -0500

    ignore files

--- libs/diffusion/config.py
+++ libs/diffusion/config.py
@@ -19,7 +19,7 @@ class BaseConfig:
 class TrainingConfig:
     TIMESTEPS = 1000 # Define number of diffusion timesteps
     IMG_SHAPE = (1, 32, 32) if BaseConfig.DATASET == "MNIST" else (1, 128, 128) 
-    NUM_EPOCHS = 10000
+    NUM_EPOCHS = 10
     BATCH_SIZE = 12
     LR = 2e-4
     NUM_WORKERS = 6
