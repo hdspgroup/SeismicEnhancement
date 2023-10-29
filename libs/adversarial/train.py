d8ca93095d1da1dd81098c731b2533388ae4a182
Brayan Monroy <brayansneider22@gmail.com>
2023-10-29 15:41:31 -0500

    add gan

---
+++ libs/adversarial/train.py
@@ -0,0 +1,145 @@
+import torch
+from libs.adversarial.utils import get_gradient, gradient_penalty, get_crit_loss, get_gen_loss, calculate_gradient_penalty
+
+def train_one_epoch(netD, netG, loader, optimizerD, optimizerG, loss_fn, epoch, device, nz):
+
+    real_label = 1.
+    fake_label = 0.
+
+    crit_repeats = 5
+    c_lambda = 10
+
+    critic_losses    = []
+    generator_losses = []
+
+
+    for i, data in enumerate(loader, 0):
+
+        mean_iteration_critic_loss = 0
+        real_images = data.to(device)
+        batch_size = real_images.size(0)
+
+        for _ in range(crit_repeats):
+            ### Update critic ###
+            noise = torch.randn(batch_size, nz, 1, 1, device=device)
+
+            ##############################################
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+            ##############################################
+            # Set discriminator gradients to zero.
+            optimizerD.zero_grad()
+
+            # Train with real
+            real_output = netD(real_images)
+            errD_real = torch.mean(real_output)
+            D_x = real_output.mean().item()
+
+            # Generate fake image batch with G
+            fake_images = netG(noise)
+
+            # Train with fake
+            fake_output = netD(fake_images.detach())
+            errD_fake = torch.mean(fake_output)
+            D_G_z1 = fake_output.mean().item()
+
+            # Calculate W-div gradient penalty
+            gradient_penalty = calculate_gradient_penalty(netD, real_images.data, fake_images.data, device)
+
+            # Add the gradients from the all-real and all-fake batches
+            errD = -errD_real + errD_fake + gradient_penalty * c_lambda
+            errD.backward()
+            # Update D
+            optimizerD.step()
+
+            mean_iteration_critic_loss += errD.item() / crit_repeats
+
+
+        critic_losses += [mean_iteration_critic_loss]
+
+
+        ### Update generator ###
+        optimizerG.zero_grad()
+
+        # Generate fake image batch with G
+        fake_images = netG(noise)
+        fake_output = netD(fake_images)
+        errG = -torch.mean(fake_output)
+        D_G_z2 = fake_output.mean().item()
+        errG.backward()
+        optimizerG.step()
+
+        # Keep track of the average generator loss
+        generator_losses += [errG.item()]
+
+
+        ## Train with all-real batch
+        # netD.zero_grad()
+        # Format batch
+        
+        # b_size = real_cpu.size(0)
+        # label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+        # # Forward pass real batch through D
+        # output = netD(real_cpu).view(-1)
+        # # Calculate loss on all-real batch
+        # errD_real = loss_fn(output, label)
+        # # Calculate gradients for D in backward pass
+        # errD_real.backward()
+        # D_x = output.mean().item()
+
+        # ## Train with all-fake batch
+        # # Generate batch of latent vectors
+        # noise = torch.randn(b_size, nz, 1, 1, device=device)
+        # # Generate fake image batch with G
+        # fake = netG(noise)
+        # label.fill_(fake_label)
+        # # Classify all fake batch with D
+        # output = netD(fake.detach()).view(-1)
+        # # Calculate D's loss on the all-fake batch
+        # errD_fake = loss_fn(output, label)
+        # # Calculate the gradients for this batch, accumulated (summed) with previous gradients
+        # errD_fake.backward()
+        # D_G_z1 = output.mean().item()
+        # # Compute error of D as sum over the fake and the real batches
+        # errD = errD_real + errD_fake
+        # # Update D
+        # optimizerD.step()
+
+        # ############################
+        # # (2) Update G network: maximize log(D(G(z)))
+        # ###########################
+        # netG.zero_grad()
+        # label.fill_(real_label)  # fake labels are real for generator cost
+        # # Since we just updated D, perform another forward pass of all-fake batch through D
+        # output = netD(fake).view(-1)
+        # # Calculate G's loss based on this output
+        # errG = loss_fn(output, label)
+        # # Calculate gradients for G
+        # errG.backward()
+        # D_G_z2 = output.mean().item()
+        # # Update G
+        # optimizerG.step()
+
+        # Output training stats
+        # if i % 50 == 0:
+        #     print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+        #           % (epoch, -1, i, len(loader),
+        #              errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        # # Save Losses for plotting later
+        # G_losses.append(errG.item())
+        # D_losses.append(errD.item())
+
+        # # Check how the generator is doing by saving G's output on fixed_noise
+        # if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+        #     with torch.no_grad():
+        #         fake = netG(fixed_noise).detach().cpu()
+        #     img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+        # iters += 1
+
+        print_val = f"Epoch: {epoch}/{-1} Steps:{i}/{len(loader)}\t"
+        print_val += f"Loss_C : {mean_iteration_critic_loss:.6f}\t"
+        print_val += f"Loss_G : {errG:.6f}\t"  
+        print(print_val, end='\r',flush = True)
+
+    return generator_losses, critic_losses
---
+++ libs/adversarial/utils.py
@@ -0,0 +1,60 @@
+import torch 
+
+def gradient_penalty(gradient):
+    gradient = gradient.view(len(gradient), -1)
+
+    gradient_norm = gradient.norm(2, dim=1)
+    
+    penalty = torch.mean((gradient_norm - 1)**2)
+    return penalty
+
+
+def get_gen_loss(crit_fake_pred):
+    gen_loss = -1. * torch.mean(crit_fake_pred)
+    return gen_loss
+
+
+def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
+    crit_loss = torch.mean(crit_fake_pred) - torch.mean(crit_real_pred) + c_lambda * gp
+    return crit_loss
+
+
+def calculate_gradient_penalty(model, real_images, fake_images, device):
+    """Calculates the gradient penalty loss for WGAN GP"""
+    # Random weight term for interpolation between real and fake data
+    alpha = torch.randn((real_images.size(0), 1, 1, 1), device=device)
+    # Get random interpolation between real and fake data
+    interpolates = (alpha * real_images + ((1 - alpha) * fake_images)).requires_grad_(True)
+
+    model_interpolates = model(interpolates)
+    grad_outputs = torch.ones(model_interpolates.size(), device=device, requires_grad=False)
+
+    # Get gradient w.r.t. interpolates
+    gradients = torch.autograd.grad(
+        outputs=model_interpolates,
+        inputs=interpolates,
+        grad_outputs=grad_outputs,
+        create_graph=True,
+        retain_graph=True,
+        only_inputs=True,
+    )[0]
+    gradients = gradients.view(gradients.size(0), -1)
+    gradient_penalty = torch.mean((gradients.norm(2, dim=1) - 1) ** 2)
+    return gradient_penalty
+
+    
+def get_gradient(crit, real, fake, epsilon):
+
+    mixed_images = real * epsilon + fake * (1 - epsilon)
+
+    mixed_scores = crit(mixed_images)
+    
+    gradient = torch.autograd.grad(
+        inputs=mixed_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores), 
+        create_graph=True,
+        retain_graph=True,
+        
+    )[0]
+    return gradient
