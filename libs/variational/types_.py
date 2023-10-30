3b8906d4e2ba1c4cd9ef86e38013dd7dd9bf569a
Brayan Monroy <brayansneider22@gmail.com>
2023-10-30 09:13:33 -0500

    Update types_.py

--- libs/variational/types_.py
+++ libs/variational/types_.py
@@ -1,3 +1,12 @@
+# -----------------------------------------------------------------------------
+# Original Source: Auto-Encoding Variational Bayes
+# GitHub Repository: AntixK/PyTorch-VAE
+# Link: https://github.com/AntixK/PyTorch-VAE
+# -----------------------------------------------------------------------------
+# Description:
+# A Collection of Variational Autoencoders (VAE) in PyTorch.
+# -----------------------------------------------------------------------------
+
 from typing import List, Callable, Union, Any, TypeVar, Tuple
 # from torch import tensor as Tensor
 
